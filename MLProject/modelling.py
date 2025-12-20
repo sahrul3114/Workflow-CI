ffbd2f53b84fad2076449092f93bbeb7238a98c5
Syahrul Akbar Ramdhani <sahrulakbar3114@gmail.com>
2025-12-20 17:39:18 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,39 +1,37 @@
 import pandas as pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import RobustScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score
 import mlflow
 import mlflow.sklearn
 
-#Load dataset
+# Load dataset
 BASE_DIR = Path(__file__).parent
 DATA_PATH = BASE_DIR / "diabetes_preprocessed.csv"
 df = pd.read_csv(DATA_PATH)
 
-# Split features & target
 X = df.drop("diabetes", axis=1)
 y = df["diabetes"].astype(int)
 
-# Train/test split
+# Train / Test Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# MLflow setup
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
-mlflow.set_experiment("Basic_Modelling")
-
-# Train model
+# Baseline Modelling
 with mlflow.start_run():
     mlflow.sklearn.autolog()
 
-    model = LogisticRegression(max_iter=500)
+    model = LogisticRegression(
+        max_iter=500,
+        class_weight="balanced",
+        random_state=42
+    )
     model.fit(X_train, y_train)
 
+    # Evaluation
     y_pred = model.predict(X_test)
-
     acc = accuracy_score(y_test, y_pred)
     f1  = f1_score(y_test, y_pred)
 
