b160d96457790a1af944de52dbaf51f2f0f69d6c
Syahrul Akbar Ramdhani <sahrulakbar3114@gmail.com>
2025-12-20 17:32:45 +0700

    Update modelling_tuning.py

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import RobustScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score
 import mlflow
@@ -15,35 +14,60 @@ df = pd.read_csv(DATA_PATH)
 X = df.drop("diabetes", axis=1)
 y = df["diabetes"].astype(int)
 
-# Train/test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Train / Test Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-# Hyperparameter tuning
+# Hyperparameter Tuning
 param_grid = [
     {"solver": ["liblinear"], "penalty": ["l1", "l2"], "C": [0.01, 0.1, 1, 10, 100]},
     {"solver": ["lbfgs"], "penalty": ["l2"], "C": [0.01, 0.1, 1, 10, 100]},
     {"solver": ["saga"], "penalty": ["l1", "l2"], "C": [0.01, 0.1, 1, 10, 100]},
-    {"solver": ["saga"], "penalty": ["elasticnet"], "l1_ratio": [0, 0.5, 1], "C": [0.01, 0.1, 1, 10, 100]}
+    {
+        "solver": ["saga"],
+        "penalty": ["elasticnet"],
+        "l1_ratio": [0, 0.5, 1],
+        "C": [0.01, 0.1, 1, 10, 100]
+    }
 ]
 
-grid = GridSearchCV(LogisticRegression(max_iter=500, class_weight='balanced', random_state=42), 
-                    param_grid, cv=5, scoring='f1', n_jobs=-1)
+grid = GridSearchCV(
+    LogisticRegression(
+        max_iter=500,
+        class_weight="balanced",
+        random_state=42
+    ),
+    param_grid,
+    cv=5,
+    scoring="f1",
+    n_jobs=-1
+)
+
 grid.fit(X_train, y_train)
 
 best_model = grid.best_estimator_
 print("Best Parameters:", grid.best_params_)
 
-mlflow.log_params(grid.best_params_)
+# MLflow Tracking 
+with mlflow.start_run():
+    # Log hyperparameters
+    mlflow.log_params(grid.best_params_)
 
-mlflow.sklearn.log_model(best_model, artifact_path="model")
-    
-y_pred = best_model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
-f1  = f1_score(y_test, y_pred)
+    # Log model
+    mlflow.sklearn.log_model(
+        best_model,
+        artifact_path="model"
+    )
 
-mlflow.log_metric("accuracy", acc)
-mlflow.log_metric("f1_score", f1)
+    # Evaluation
+    y_pred = best_model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    f1  = f1_score(y_test, y_pred)
 
-print("Accuracy:", acc)
+    # Log metrics
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("f1_score", f1)
 
-print("F1 score:", f1)
+    print("Accuracy:", acc)
+    print("F1 Score:", f1)
